ca82e5e1bc9603f34d484d88d67ddfc876d5ac41
canto9112 <cantoweb112@gmail.com>
2021-08-03 15:45:49 +0300

    Запретил менять дату объявления

--- property/admin.py
+++ property/admin.py
@@ -5,6 +5,9 @@ from .models import Flat
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('owner', 'town', 'address', )
+    readonly_fields = ["created_at"]
 
 
 admin.site.register(Flat, FlatAdmin)
+
+
